3929df775f859e31717aad6116ff634034f4fdf1
jugal-ahir <154867489+jugal-ahir@users.noreply.github.com>
2025-01-20 18:56:30 +0530

    Update DisplayLottie.tsx

--- components/DisplayLottie.tsx
+++ components/DisplayLottie.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import Lottie from "react-lottie";
+import dynamic from "next/dynamic";
 
 type Props = {
   animationPath: string;
 };
 
+// Dynamically import react-lottie to disable server-side rendering
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
+
 const GreetingLottie = ({ animationPath }: Props) => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    path: animationPath,
+    animationData: animationPath, // Provide the animation JSON object or path here
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   return (
     <div onClick={() => null}>
-      {/* @ts-ignore */}
+      {/* Lottie animation */}
       <Lottie options={defaultOptions} />
     </div>
   );
