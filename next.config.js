77420f04fbff28fab9047438471c5a49a043337e
jugal-ahir <154867489+jugal-ahir@users.noreply.github.com>
2025-01-20 18:43:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
     domains: ["avatars.githubusercontent.com"],
   },
   output: "standalone",
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Prevents lottie-web from being bundled during server-side rendering
+      config.externals.push({
+        "lottie-web": "lottie-web",
+      });
+    }
+    return config;
+  },
 };
